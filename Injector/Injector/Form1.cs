cb674af2fb135de21277d3b03e3cc4271b38010c
방수현 <bangs5282@gmail.com>
2022-02-15 16:07:01 +0900

    .

---
+++ Injector/Injector/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Injector
+{
+    public partial class Form1 : Form
+    {
+        string path = System.Windows.Forms.Application.StartupPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Inject
+        {
+            System.Diagnostics.ProcessStartInfo pri = new System.Diagnostics.ProcessStartInfo();
+            System.Diagnostics.Process pro = new System.Diagnostics.Process();
+
+            pri.FileName = "cmd.exe";
+
+            pri.CreateNoWindow = true;
+            pri.UseShellExecute = false;
+
+            pri.RedirectStandardInput = true;
+            pri.RedirectStandardOutput = true;
+            pri.RedirectStandardError = true;
+
+            pro.StartInfo = pri;
+            pro.Start();
+
+            pro.StandardInput.Write(@"cd" + path + Environment.NewLine);
+            pro.StandardInput.Write(@"smi.exe inject -p TheVanOfJustice -a VanOfJusticeCheatDll.dll -n Gamename_Hack -c Loader -m Init" + Environment.NewLine);
+            pro.StandardInput.Close();
+
+            string resultValue = pro.StandardOutput.ReadToEnd();
+
+            pro.WaitForExit();
+            pro.Close();
+
+            MessageBox.Show("Injected!");
+            this.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e) // FindDll
+        {
+            bool isGameRunning = false;
+            Process[] proccesses = Process.GetProcesses();
+
+            foreach (Process p in proccesses)
+            {
+                if (p.ProcessName == "TheVanOfJustice")
+                {
+                    isGameRunning = true;
+                }
+            }
+
+            if (!isGameRunning)
+            {
+                MessageBox.Show("게임이 실행되고있지 않습니다\n게임을 실행한뒤 실행해주세요");
+            }
+
+            bool SharpMonoInjector = false;
+            bool VanOfJusticeCheatDll = false;
+            bool smi = false;
+
+            string[] files = Directory.GetFiles(@path, "*.dll");
+            foreach (var file in files)
+            {
+                if (file.ToString() == path + "\\SharpMonoInjector.dll")
+                {
+                    SharpMonoInjector = true;
+                }
+
+                if (file.ToString() == path + "\\VanOfJusticeCheatDll.dll")
+                {
+                    VanOfJusticeCheatDll = true;
+                }
+            }
+
+            files = Directory.GetFiles(@path, "*.exe");
+            foreach (var file in files)
+            {
+                if (file.ToString() == path + "\\smi.exe")
+                {
+                    smi = true;
+                }
+            }
+
+            if (!SharpMonoInjector)
+            {
+                MessageBox.Show("SharpMonoInjector.dll 파일을 찾을수 없습니다\n폴더에 파일이 있는지 확인해주세요");
+            }
+            if (!VanOfJusticeCheatDll)
+            {
+                MessageBox.Show("VanOfJusticeCheatDll.dll 파일을 찾을수 없습니다\n폴더에 파일이 있는지 확인해주세요");
+            }
+            if (!smi)
+            {
+                MessageBox.Show("smi.exe 파일을 찾을수 없습니다\n폴더에 파일이 있는지 확인해주세요");
+            }
+
+            if(SharpMonoInjector && VanOfJusticeCheatDll && smi && isGameRunning)
+            {
+                button2.Enabled = false;
+                button1.Enabled = true;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/bangs5282");
+        }
+    }
+}
---
+++ VanOfJusticeCheatDll/VanOfJusticeCheatDll/Loader.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+
+namespace Gamename_Hack
+{
+    class Main : MonoBehaviour
+    {
+        string debug = "Made by Github bangs5282";
+
+        bool menu = true;
+
+        bool god_mode = false;
+
+        bool infinitRPG = false;
+
+        int SpeedCheat = -1;
+
+        GameObject gb;
+        Movement mv;
+        Combat cb;
+
+        GameObject rocketlaunch;
+        RPG7 rpg;
+
+        public void Start()
+        {
+            //mv.speed = 0;
+        }
+        public void Update()
+        {
+            gb = GameObject.FindGameObjectWithTag("Player");
+            mv = gb.GetComponent<Movement>();
+            cb = gb.GetComponent<Combat>();
+
+            try
+            {
+                rocketlaunch = GameObject.Find("Toy Rocket Launccher(Clone)");
+                rpg = rocketlaunch.GetComponent<RPG7>();
+            }
+            catch { }
+
+            if (Input.GetKeyDown(KeyCode.End))
+            {
+                if (menu)
+                {
+                    menu = false;
+                }
+                else
+                {
+                    menu = true;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Delete))
+            {
+                Loader.Unload();
+            }
+
+            if (Input.GetKeyDown(KeyCode.PageUp))
+            {
+                if (god_mode)
+                {
+                    god_mode = false;
+                }
+                else
+                {
+                    god_mode = true;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.PageDown))
+            {
+                if (infinitRPG)
+                {
+                    infinitRPG = false;
+                }
+                else
+                {
+                    infinitRPG = true;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Insert))
+            {
+                Movement.currentMoney = 999999;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Home))
+            {
+                SpeedCheat *= -1;
+            }
+
+            if (SpeedCheat == 1)
+            {
+                mv.speed = 50f;
+            }
+            else if (SpeedCheat == -1)
+            {
+                mv.speed = 8.2f;
+            }
+
+            if (god_mode)
+            {
+                cb.hp = 100;
+            }
+            else
+            {
+                cb.fullHp = 100;
+            }
+
+            if (infinitRPG)
+            {
+                rpg.amount = 9;
+            }
+
+
+        }
+        public void OnGUI()
+        {
+            if (menu)
+            {
+                float offsetx = 340f;
+                float offsety = 0f;
+
+                GUI.Box(new Rect(10f + offsetx, 10f + offsety, 265f, 115f), "Press [End] to Close Menu");
+
+
+                GUI.Label(new Rect(15f + offsetx, 30f + offsety, 300f, 50f), "[Insert] - Money");
+                GUI.Label(new Rect(15f + offsetx, 45f + offsety, 300f, 50f), "[Home] - Speed");
+                GUI.Label(new Rect(15f + offsetx, 60f + offsety, 300f, 50f), "[PgUp] - GodMode");
+                GUI.Label(new Rect(15f + offsetx, 75f + offsety, 300f, 50f), "[PgDn] - Infinite Rocket");
+                GUI.Label(new Rect(15f + offsetx, 95f + offsety, 300f, 50f), debug);
+            }
+        }
+
+
+    }
+    public class Loader
+    {
+        static GameObject _Load;
+
+        public static void Init()
+        {
+            _Load = new GameObject();
+            _Load.AddComponent<Main>();
+            GameObject.DontDestroyOnLoad(_Load);
+        }
+        public static void Unload()
+        {
+            _Unload();
+        }
+        private static void _Unload()
+        {
+            GameObject.Destroy(_Load);
+        }
+        private GameObject _gameObject;
+    }
+}
